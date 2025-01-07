f186584f421e2415773f0ee661916e69eb994b8b
Kirill <128813627+SkKirill@users.noreply.github.com>
2025-01-08 01:28:14 +0300

    Correct SelectPathsViewModel

--- DocumentGenerator.UI/ViewModels/UserControlsViewModel/SelectPathsViewModel.cs
+++ DocumentGenerator.UI/ViewModels/UserControlsViewModel/SelectPathsViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Reactive;
 using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media;
 using ReactiveUI;
 
@@ -8,79 +13,276 @@ namespace DocumentGenerator.UI.ViewModels.UserControlsViewModel;
 
 public class SelectPathsViewModel : ViewModelBase, IUserControlsNotifier
 {
-    private IImmutableSolidColorBrush _colorValidDataPath;
-
-    public IImmutableSolidColorBrush ColorValidDataPath
-    {
-        get => _colorValidDataPath;
-        set => this.RaiseAndSetIfChanged(ref _colorValidDataPath, value);
-    }
-    private IImmutableSolidColorBrush _colorValidSaveDataFolder;
-
-    public IImmutableSolidColorBrush ColorValidSaveDataFolder
-    {
-        get => _colorValidSaveDataFolder;
-        set => this.RaiseAndSetIfChanged(ref _colorValidSaveDataFolder, value);
-    }
-    
+    /// <summary>
+    /// Событие по переходу на следующую или предыдущую форму
+    /// </summary>
     public IObservable<bool> CompleteView => _completeView;
-    public string ValidTextLabelDataPath { get; set; }
-    public string ValidTextLabelFolderPath { get; set; }
-    public ReactiveCommand<Unit, Unit> OpenSearchPath { get; }
-    public ReactiveCommand<Unit, Unit> OpenSearchFolder { get; }
-    public ReactiveCommand<Unit, Unit> ClearActionButton { get; }
-    public ReactiveCommand<Unit, Unit> ContinueButton { get; }
-    public string DataLocationTextBoxTextChanged
+
+    /// <summary>
+    /// Цвет строки подсказки под полем ввода пути к файлу с данными
+    /// </summary>
+    public IImmutableSolidColorBrush HelpLabelSearchPathColor
     {
-        get => _dataLocationTextBoxTextChanged;
-        set => _dataLocationTextBoxTextChanged = value;
-    }
-    public string SaveDataLocationTextBoxTextChanged
-    {
-        get => _saveFolderDataLocationTextBoxTextChanged;
-        set => _saveFolderDataLocationTextBoxTextChanged = value;
+        get => _helpLabelSearchPathColor; 
+        set => this.RaiseAndSetIfChanged(ref _helpLabelSearchPathColor, value);
     }
     
-    private string _dataLocationTextBoxTextChanged;
-    private string _saveFolderDataLocationTextBoxTextChanged;
+    /// <summary>
+    /// Цвет строки подсказки под полем ввода папки для сохранения файлов
+    /// </summary>
+    public IImmutableSolidColorBrush HelpLabelSearchFolderColor
+    {
+        get => _helpLabelSearchFolderColor;
+        set => this.RaiseAndSetIfChanged(ref _helpLabelSearchFolderColor, value); 
+    }
+
+    /// <summary>
+    /// Строка с подсказкой о существовании пути
+    /// </summary>
+    public string HelpLabelSearchPath
+    {
+        get => _helpLabelSearchPath; 
+        set => this.RaiseAndSetIfChanged(ref _helpLabelSearchPath, value);
+    }
+
+    /// <summary>
+    /// Строка подсказки о существовании папки
+    /// </summary>
+    public string HelpLabelSearchFolder
+    {
+        get => _helpLabelSearchFolder; 
+        set => this.RaiseAndSetIfChanged(ref _helpLabelSearchFolder, value);
+    }
     
-    private Subject<bool> _completeView;
+    /// <summary>
+    /// Строка с расположением файлов с исходными данными
+    /// </summary>
+    public string LocationDataText
+    {
+        get => _locationDataText;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _locationDataText, value);
+            ValidatePath(_locationDataText);
+        }
+    }
+
+    /// <summary>
+    /// Строка с указанием папки для сохранения файлов
+    /// </summary>
+    public string LocationFolderSaveText
+    {
+        get => _locationFolderSaveText;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _locationFolderSaveText, value);
+            ValidateDirectory(_locationFolderSaveText);
+        }
+    }
+    
+    /// <summary>
+    /// Команда при нажатии на кнопку поиска пути (лупа)
+    /// </summary>
+    public ReactiveCommand<Unit, Task> OpenSearchPathCommand { get; }
+
+    /// <summary>
+    /// Команда при нажатии на кнопку поиска папки (лупа)
+    /// </summary>
+    public ReactiveCommand<Unit, Task> OpenSearchFolderCommand { get; }
+
+    /// <summary>
+    /// Команда при нажатии на кнопку "Вернуться"
+    /// </summary>
+    public ReactiveCommand<Unit, Unit> GoBackActionCommand { get; }
+
+    /// <summary>
+    /// Команда при нажатии на кнопку "Продолжить"
+    /// </summary>
+    public ReactiveCommand<Unit, Unit> ContinueActionCommand { get; }
+    
+    private IImmutableSolidColorBrush _helpLabelSearchPathColor = null!;
+    private IImmutableSolidColorBrush _helpLabelSearchFolderColor = null!;
+    
+    private string _helpLabelSearchPath = null!;
+    private string _helpLabelSearchFolder = null!;
+
+    private string _locationDataText = null!;
+    private string _locationFolderSaveText = null!;
+    private readonly Subject<bool> _completeView;
+
+    [Obsolete("Obsolete")]
     public SelectPathsViewModel()
     {
         _completeView = new Subject<bool>();
-        _saveFolderDataLocationTextBoxTextChanged = string.Empty;
-        _dataLocationTextBoxTextChanged = string.Empty;
+        LocationFolderSaveText = string.Empty;
+        LocationDataText = string.Empty;
+
+        OpenSearchPathCommand = ReactiveCommand.Create(RunSearchPath);
+        OpenSearchFolderCommand = ReactiveCommand.Create(RunSearchFolder);
+        GoBackActionCommand = ReactiveCommand.Create(RunClearAction);
+        ContinueActionCommand = ReactiveCommand.Create(RunContinue);
+
+        HelpLabelSearchPath = "*такого файла не существует";
+        HelpLabelSearchFolder = "*некорректно указана папка для создания";
         
-        ValidTextLabelDataPath = "*такого файла не существует";
-        ValidTextLabelFolderPath = "*некорректно указана папка для создания";
-        
-        OpenSearchPath = ReactiveCommand.Create(RunSearchPath);
-        OpenSearchFolder = ReactiveCommand.Create(RunSearchFolder);
-        ClearActionButton = ReactiveCommand.Create(RunClearAction);
-        ContinueButton = ReactiveCommand.Create(RunContinue);
-        
-        ColorValidSaveDataFolder = Brushes.Red;
-        ColorValidDataPath = Brushes.Red;
+        HelpLabelSearchFolderColor = Brushes.Red;
+        HelpLabelSearchPathColor = Brushes.Red;
     }
 
     private void RunContinue()
     {
-        _completeView.OnNext(true);
+        var success = true;
+        if (!ValidatePath(_locationDataText))
+        {
+            HelpLabelSearchPath = "*требуется указать путь к файлу с данными";
+            HelpLabelSearchPathColor = Brushes.Red;
+            success = false;
+        }
+
+        if (!ValidateDirectory(_locationFolderSaveText))
+        {
+            try
+            {
+                Directory.CreateDirectory(_locationFolderSaveText);
+            }
+            catch
+            {
+                HelpLabelSearchFolder = "*не удалось создать папку..";
+                HelpLabelSearchFolderColor = Brushes.Red;
+                success = false;
+            }
+        }
+
+        if (success)
+        {
+            _completeView.OnNext(true);
+        }
     }
 
+    /// <summary>
+    /// Обработка нажатия кнопки вернуться (после чего произойдет возврат на прошлую форму)
+    /// </summary>
     private void RunClearAction()
     {
         _completeView.OnNext(false);
-        // TODO: обработка нажатия в окошко поиска директории
     }
 
-    private void RunSearchFolder()
+    /// <summary>
+    /// Обработка нажатия кнопки поиска папки
+    /// </summary>
+    [Obsolete("Obsolete")]
+    private async Task RunSearchFolder()
     {
-        // TODO: обработка нажатия в окошко поиска директории
-    }
-    private void RunSearchPath()
-    {
-        // TODO: обработка нажатия в окошко поиска пути
+        var openFolderDialog = new OpenFolderDialog();
+
+        if (Application.Current!.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var window = desktop.MainWindow;
+            if (window == null)
+            {
+                HelpLabelSearchFolder = "*не удалось открыть диалоговое окно";
+                HelpLabelSearchFolderColor = Brushes.Red;
+                return;
+            }
+
+            var result = await openFolderDialog.ShowAsync(window);
+            if (!string.IsNullOrEmpty(result))
+            {
+                LocationFolderSaveText = result;
+            }
+
+            ValidateDirectory(LocationFolderSaveText);
+        }
     }
 
+    /// <summary>
+    /// Обработка нажатия кнопки поиска пути к файлу
+    /// </summary>
+    [Obsolete("Obsolete")]
+    private async Task RunSearchPath()
+    {
+        var openFileDialog = new OpenFileDialog()
+        {
+            AllowMultiple = false,
+            Filters =
+            [
+                new FileDialogFilter
+                {
+                    Name = "Базы данных и Excel",
+                    Extensions = { "xlsx", "xls", "db", "sqlite", "mdb", "accdb" }
+                }
+            ]
+        };
+
+        if (Application.Current!.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var window = desktop.MainWindow;
+            if (window == null)
+            {
+                HelpLabelSearchPath = "*не удалось открыть диалоговое окно";
+                HelpLabelSearchPathColor = Brushes.Red;
+                return;
+            }
+
+            var result = await openFileDialog.ShowAsync(window);
+            if (result is { Length: > 0 })
+            {
+                LocationDataText = result[0];
+            }
+
+            ValidatePath(LocationDataText);
+        }
+    }
+
+    /// <summary>
+    /// Проверка на существование пути к файлу с данными
+    /// </summary>
+    /// <param name="path">Путь к файлу с данными</param>
+    /// <returns>True -> если такой файл существует</returns>
+    private bool ValidatePath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            HelpLabelSearchPath = "*путь не указан";
+            HelpLabelSearchPathColor = Brushes.Red;
+            return false;
+        }
+
+        if (File.Exists(path))
+        {
+            HelpLabelSearchPath = "*файл найден";
+            HelpLabelSearchPathColor = Brushes.Green;
+            return true;
+        }
+
+        HelpLabelSearchPath = "*неверно указан путь к файлу";
+        HelpLabelSearchPathColor = Brushes.Red;
+        return false;
+    }
+
+    /// <summary>
+    /// Проверка существования папки
+    /// </summary>
+    /// <param name="path">Путь к папке</param>
+    /// <returns>True -> если папка существует</returns>
+    private bool ValidateDirectory(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            HelpLabelSearchFolder = "*путь к папке не указан";
+            HelpLabelSearchFolderColor = Brushes.Red;
+            return false;
+        }
+
+        if (Directory.Exists(path))
+        {
+            HelpLabelSearchFolder = "*папка найдена";
+            HelpLabelSearchFolderColor = Brushes.Green;
+            return true;
+        }
+
+        HelpLabelSearchFolder = "*будет попытка создать данную папку";
+        HelpLabelSearchFolderColor = Brushes.Orange;
+        return false;
+    }
 }
